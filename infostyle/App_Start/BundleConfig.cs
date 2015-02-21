b14b4af3e200654e9551710600c026ab2f15cdc7
Valeriy 'Malcoriel' Kuzmin <malcoriel@gmail.com>
2015-02-21 16:31:05 +0500

    crawlLine as polymer element

--- infostyle/App_Start/BundleConfig.cs
+++ infostyle/App_Start/BundleConfig.cs
@@ -17,17 +17,15 @@ namespace infostyle
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
-            bundles.Add(new DynamicFolderBundle("polymer", "*.html", true)
-                .Include("~/Scripts/polymer"));
-
-
-
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css")
+                .Include("~/Content/site.css")
+                .Include("~/Content/App.css")
+                );
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
--- infostyle/Scripts/App/api.js
+++ infostyle/Scripts/App/api.js
@@ -1,8 +1,21 @@
 ﻿window.Api = {
-    address: "http://K1307004.kontur:4444/api",
+    address: "http://norris.kontur:4444/api",
     GetTask: function(type) {
-        return mockDef(["пыщь","бздыщь","херыщь"]);
-        //return $.get(Api.address + "/GetTask", { type: type });
+        return mockDef({
+            Type: TaskTypes.CrawlLine,
+            Data: {
+                Text: [
+                    { Text: "В рамках", Type: "Stop" },
+                    { Text: "ежегодного фестиваля техники", Type: "Normal" },
+                    { Text: "продемонстрировали", Type: "Normal" },
+                    { Text: "свыше", Type: "Stop" },
+                    { Text: "ста", Type: "Normal" },
+                    { Text: "совершенно новых", Type: "Stop" },
+                    { Text: "экспонатов", Type: "Normal" }
+                ]
+            }
+        });
+        //return $.get(Api.address + "/task/get", { type: type });
     }
 };
 
@@ -15,3 +28,8 @@ window.mockDef = function(result) {
 window.TaskTypes = {
     CrawlLine: "CrawlLine"
 };
+
+window.TextTypes = {
+    Normal: "Normal",
+    Stop : "Stop"
+}
