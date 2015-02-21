b164c9d46096a9cd09a3333ab7c46024cf133f82
unknown <a.ponomarev@K1307005.kontur>
2015-02-21 14:48:55 +0500

    Отдаем таск

---
+++ Api/Controllers/TaskController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web.Http;
+using Api.Models;
+
+namespace Api.Controllers
+{
+    public class TaskController : ApiController
+    {
+        public GameTask Get()
+        {
+            return new GameTask
+            {
+                Id = Guid.NewGuid(),
+                Type = TaskType.CrawlLine,
+                Text = new[]
+                {
+                    new Phrase {Text = "Все в порядке.", Type = TextType.Normal},
+                    new Phrase {Text = "СТОП!", Type = TextType.Stop},
+                    new Phrase {Text = "И снова все в порядке.", Type = TextType.Normal}
+                }
+            };
+        }
+    }
+}
---
+++ Api/Models/GameTask.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Api.Models
+{
+    public class GameTask
+    {
+        public Guid Id { get; set; }
+        public TaskType Type { get; set; }
+        public Phrase[] Text { get; set; }
+    }
+
+    public enum TaskType
+    {
+        CrawlLine,
+    }
+
+    public class Phrase
+    {
+        public string Text { get; set; }
+        public TextType Type { get; set; }
+    }
+
+    public enum TextType
+    {
+        Normal,
+        Stop
+    }
+}
