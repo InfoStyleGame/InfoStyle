b7054cc86e8ecd70b387fb4c96fa75270757437b
pesterev da <pesterev_da@skbkontur.ru>
2015-02-21 20:23:51 +0500

    Тестовый вход через vk

---
+++ infostyle/Controllers/LoginController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace infostyle.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
