2a686578c6a6192b330a1052dde0cf19708d2652
pesterev da <pesterev_da@skbkontur.ru>
2015-02-21 14:00:10 +0500

    qqq

---
+++ Api/Helpers/VkAuthHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Api.Helpers
+{
+    public class VkAuthHelper
+    {
+        public static string GetCurrentUserId(HttpRequestHeaders requestParams)
+        {
+            const string cookieKey = "vk_app_" + VkAuthParameters.VkAppId;
+            var cookie = requestParams.GetCookies(cookieKey).FirstOrDefault();
+            if (cookie == null)
+                return null;
+
+            try
+            {
+                var parameters = VkAuthParameters.Parse(cookie[cookieKey].Value);
+                VerifyParameters(parameters);
+                return parameters.UserId;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        private static void VerifyParameters(VkAuthParameters parameters)
+        {
+            if (parameters.Sig != GetMd5Hash(parameters.StringForMd5))
+                throw new Exception(string.Format("Bad Md5. Sig: {0} , StringFormMD5: {1}.", parameters.Sig, parameters.StringForMd5));
+        }
+
+        private static string GetMd5Hash(string s)
+        {
+            var md5Hasher = MD5.Create();
+            var data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(s));
+            var sBuilder = new StringBuilder();
+            foreach (var t in data)
+                sBuilder.Append(t.ToString("x2"));
+            return sBuilder.ToString();
+        }
+    }
+}
---
+++ Api/Helpers/VkAuthParameters.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace Api.Helpers
+{
+    public class VkAuthParameters
+    {
+        public const string VkAppId = "4793375";
+        public const string SecretString = "lDJqz0Cr2Q3FZqZ6P0MN";
+
+        public string Expire { get; set; } 
+        public string UserId { get; set; }
+        public string Secret { get; set; }
+        public string SessionId { get; set; }
+        public string Sig { get; set; }
+        public string StringForMd5 { get; set; }
+
+        public static VkAuthParameters Parse(string s)
+        {
+            var parameters = s.Split('&').Select(p => p.Split('=')).ToDictionary(p => p[0], p => p[1]);
+            var result = new VkAuthParameters
+                {
+                    Expire = parameters["expire"],
+                    Secret = parameters["secret"],
+                    SessionId = parameters["sid"],
+                    Sig = parameters["sig"],
+                    UserId = parameters["mid"]
+                };
+            result.StringForMd5 = String.Format("expire={0}mid={1}secret={2}sid={3}{4}", result.Expire,
+                                                result.UserId, result.Secret, result.SessionId, SecretString);
+            return result;
+        }
+    }
+}
