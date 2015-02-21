459f5d1713f7856f7623f9453242f3972ff577dd
aponomarev <none@none>
2015-02-21 18:45:25 +0500

    FillTestStorage

---
+++ EF/FillTestStorage.sql
@@ -0,0 +1,19 @@
+﻿use Infostyle
+
+truncate table Tasks
+
+insert into Tasks (id, type, text) values (newid(), 0, '{Общеизвестно}, что крысы покидают судно, если в трюмах поднимается вода.')
+insert into Tasks (id, type, text) values (newid(), 0, '{Ни для кого не секрет}, что причина комплексов взрослого кроется в глубоком детстве.')
+insert into Tasks (id, type, text) values (newid(), 0, 'Большая зеленая кнопка, {очевидно}, самый распространенный элемент интерфейсов в e-commerce')
+insert into Tasks (id, type, text) values (newid(), 0, 'В состав холдинга {также} входят три торговых порта, собственные транспортные операторы, сбытовые и сервисные сети.')
+insert into Tasks (id, type, text) values (newid(), 0, 'Этим примером нам хотелось {как бы} проиллюстрировать ситуацию на рынке труда.')
+insert into Tasks (id, type, text) values (newid(), 0, 'Карлсберг, {пожалуй}, лучшее пиво в мире.')
+insert into Tasks (id, type, text) values (newid(), 0, 'Новогоднюю ёлку, {впрочем}, выносят до майских праздников.')
+insert into Tasks (id, type, text) values (newid(), 0, 'Ты {хотя бы} встань с постели, яичницу приготовь!')
+insert into Tasks (id, type, text) values (newid(), 0, 'Маленький Поросёнок Пётр хочет воспользоваться предметом или сооружением, которого у него, {разумеется}, нет.')
+insert into Tasks (id, type, text) values (newid(), 0, 'Компания Добрая корова, {как нам кажется}, показала лучшую презентацию фестиваля сельского хозяйства.')
+insert into Tasks (id, type, text) values (newid(), 0, 'Креатив, {по заверению Алексея}, говно, автор - мудак')
+insert into Tasks (id, type, text) values (newid(), 0, '{Я считаю}, пора законодательно запретить носить сланцы на носки')
+insert into Tasks (id, type, text) values (newid(), 0, '{Кажется}, когда кажется - креститься надо.')
+insert into Tasks (id, type, text) values (newid(), 0, '{Если говорить про меня, то} я за легализацию лёгких наркотиков.')
+insert into Tasks (id, type, text) values (newid(), 0, '{Думаю}, это не игра, а проверка грамотности от жидо-масонов.')
