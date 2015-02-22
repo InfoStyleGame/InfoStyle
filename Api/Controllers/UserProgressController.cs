9ae7002d2150b5eeff74fce8e5e122fe6761e381
aponomarev <none@none>
2015-02-22 15:53:35 +0500

    Отдаем уровень 1 новичкам

--- Api/Controllers/UserProgressController.cs
+++ Api/Controllers/UserProgressController.cs
@@ -7,12 +7,16 @@ namespace Api.Controllers
 {
     public class UserProgressController : ControllerBase
     {
+        private const int defaultLevel = 1;
+
         public UserProgress Get()
         {
             var userId = VkAuthHelper.GetCurrentUser(Request.Headers).Id;
             using (var context = new InfostyleEntities())
             {
-                return new UserProgress {CurrentLevel = context.Scores.Where(s => s.UserId == userId).Max(s => s.Level)};
+                var scores = context.Scores.Where(s => s.UserId == userId).ToArray();
+                var currentLevel = scores.Any() ? scores.Max(s => s.Level) : defaultLevel;
+                return new UserProgress {CurrentLevel = currentLevel};
             }
         }
     }
