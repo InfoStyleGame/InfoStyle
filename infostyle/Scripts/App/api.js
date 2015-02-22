96ddcc8dcc029138caca3cee87d1adbf22197262
Valeriy 'Malcoriel' Kuzmin <malcoriel@gmail.com>
2015-02-22 14:24:55 +0500

    api

--- infostyle/Scripts/App/api.js
+++ infostyle/Scripts/App/api.js
@@ -1,36 +1,17 @@
 ﻿window.Api = {
     root: "http://norris.kontur:4444/api",
     GetTask: function (type, subject) {
-        return $.get(Api.root + "/task", { type: type, subject: subject })
+        return $.get(Api.root + "/task/get", { type: type, subject: subject })
             .fail(function(data) { console.log("Error: " + data); });
     },
 
     GetCards: function() {
-        return mockDef({
-            Id: "mockId",
-            Data: [
-                [
-                    { Text: "1В рамках ежегодного фестиваля техники продемонстрировали ", Type: TextTypes.Normal },
-                    { Text: "свыше ", Type: TextTypes.Stop },
-                    { Text: "ста совершенно новых экспанатов", Type: TextTypes.Normal }
-                ],
-                [
-                    { Text: "2В рамках ежегодного фестиваля техники продемонстрировали ", Type: TextTypes.Normal },
-                    { Text: "свыше ", Type: TextTypes.Stop },
-                    { Text: "ста совершенно новых экспанатов", Type: TextTypes.Normal }
-                ],
-                [
-                    { Text: "3В рамках ежегодного фестиваля техники продемонстрировали ", Type: TextTypes.Normal },
-                    { Text: "свыше ", Type: TextTypes.Stop },
-                    { Text: "ста совершенно новых экспанатов", Type: TextTypes.Normal }
-                ],
-                
-            ]
-    })
+        return $.post(Api.root + "/task/card?level=0")
+            .fail(function (data) { console.log("Error: " + data); });
     },
 
     SubmitTaskResults: function(score, taskId) {
-        return $.post(Api.root + "/taskResult?score=" + score + "&taskId=" + taskId);
+        return $.post(Api.root + "/taskResult/post?score=" + score + "&taskId=" + taskId);
     },
 
     GetUserInfo: function() {
