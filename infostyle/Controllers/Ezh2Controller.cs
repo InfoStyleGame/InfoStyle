47b960d6a49130a3d07c2a6c3c07c488c30040ae
Valeriy 'Malcoriel' Kuzmin <malcoriel@gmail.com>
2015-02-22 18:23:14 +0500

    more markup

---
+++ infostyle/Controllers/Ezh2Controller.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace infostyle.Controllers
+{
+    public class Ezh2Controller : ControllerBase
+    {
+        public ActionResult Index()
+        {
+            VerifyIsLoggedIn();
+            return View();
+        }
+    }
+}
---
+++ infostyle/Controllers/EzhController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace infostyle.Controllers
+{
+    public class EzhController : ControllerBase
+    {
+        public ActionResult Index()
+        {
+            VerifyIsLoggedIn();
+            return View();
+        }
+    }
+}
