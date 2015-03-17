b2fdb5cd8ec8d0cb063dbd5d30f22bf3d1201c34
Valeriy Kuzmin <vkuzmin@skbkontur.ru>
2015-03-17 14:41:07 +0500

    page layout using polymer: see /Static/Index.html for example

--- infostyle/App_Start/BundleConfig.cs
+++ infostyle/App_Start/BundleConfig.cs
@@ -10,18 +10,6 @@ namespace infostyle
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-2.1.3.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-1.8.20.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
             bundles.Add(new StyleBundle("~/Content/css")
                 .Include("~/Content/site.css")
                 .Include("~/Content/App.css")
