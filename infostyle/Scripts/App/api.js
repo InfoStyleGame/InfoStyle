4de7ca3aa7d5c837b263b34ef14b0a77b10ab238
pesterev da <pesterev_da@skbkontur.ru>
2015-03-22 16:36:41 +0500

    theCakeIsALie! = моковое апи готово Чтобы пользоваться моковым апи нужно сделать правдивой константу theCakeIsALie в api.js

--- infostyle/Scripts/App/api.js
+++ infostyle/Scripts/App/api.js
@@ -1,25 +1,41 @@
 ﻿window.Api = {
     root: "/api",
+    theCakeIsALie: false,
     GetTask: function (type, level) {
+        if (Api.theCakeIsALie) {
+            return mockDef(mockApi.GetTask_Result);
+        }
         level = level - 1;
         return this._DoApiCall("/task/get?level=" + level + "&type=" + type, 'POST');
     },
 
     GetCards: function (level) {
         level = level - 1;
+        if (Api.theCakeIsALie) {
+            return mockDef(mockApi.GetCards_Result);
+        }
         return this._DoApiCall("/task/card?level=" + level, 'POST');
     },
 
     SubmitTaskResults: function (level, score) {
         level = level - 1;
+        if (Api.theCakeIsALie) {
+            return mockDef();
+        }
         return this._DoApiCall("/taskResult/post?score=" + score + "&level=" + level, 'POST');
     },
 
-    GetUserInfo: function() {
+    GetUserInfo: function () {
+        if (Api.theCakeIsALie) {
+            return mockDef(mockApi.GetUserInfo_Result);
+        }
         return this._DoApiCall("/userInfo/get", 'GET');
     },
 
-    GetUserProgress: function() {
+    GetUserProgress: function () {
+        if (Api.theCakeIsALie) {
+            return mockDef(mockApi.GetUserProgress_Result);
+        }
         return this._DoApiCall("/userProgress/get", 'get');
     },
 
@@ -42,6 +58,52 @@ window.mockDef = function(result) {
     return $def.promise();
 };
 
+window.mockApi = {
+    GetCards_Result: [
+        {
+            "Id": "0318927b-b955-40dc-9513-2373138e0d40",
+            "Type": "Card",
+            "Data": {
+                "Text": [
+                    { "Text": "Ни для кого не секрет", "Type": "Stop" },
+                    { "Text": ", что причина комплексов взрослого кроется в глубоком детстве.", "Type": "Normal" }]
+            }
+        }, {
+            "Id": "726c8e80-b955-40dc-9513-2373138e0d40",
+            "Type": "Card",
+            "Data": {
+                "Text": [
+                    { "Text": "Общеизвестно", "Type": "Stop" },
+                    { "Text": ", что крысы покидают судно, если в трюмах поднимается вода.", "Type": "Normal" }]
+            }
+        }, {
+            "Id": "85218c97-9767-4d68-90b5-af782e69f096",
+            "Type": "Card",
+            "Data": {
+                "Text": [
+                    { "Text": "Новогоднюю ёлку, ", "Type": "Normal" },
+                    { "Text": "впрочем", "Type": "Stop" }, { "Text": ", выносят до майских праздников.", "Type": "Normal" }]
+            }
+        }],
+    GetUserInfo_Result: {
+        "Name": "Максим Ильяхов",
+        "PictureSrc": "/Content/resources/avatar.png",
+        "Score": 250
+    },
+    GetTask_Result: {
+        "Id": "4a42d787-f7fc-4ed5-8c60-d2a286962704",
+        "Type": "CrawlLine",
+        "Data": {
+            "Text": [
+                { "Text": "Безмятежный пуховый дятел живет, ", "Type": "Normal" },
+                { "Text": "как правило", "Type": "Stop" },
+                { "Text": ", в зарослях ландышей и незабудок. Он хитёр!", "Type": "Normal" }
+            ]
+        }
+    },
+    GetUserProgress_Result: { "CurrentLevel": 3 }
+}
+
 window.TaskTypes = {
     CrawlLine: "CrawlLine"
 };
@@ -49,4 +111,4 @@ window.TaskTypes = {
 window.TextTypes = {
     Normal: "Normal",
     Stop : "Stop"
-}
+};
