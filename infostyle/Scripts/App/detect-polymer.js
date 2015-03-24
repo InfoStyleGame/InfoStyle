54d8d5cb44631093d9197d26c7ab1f6b001f0d5d
Valeriy Kuzmin <vkuzmin@skbkontur.ru>
2015-03-24 11:27:47 +0500

    IE9 detection.

--- infostyle/Scripts/App/detect-polymer.js
+++ infostyle/Scripts/App/detect-polymer.js
@@ -1,4 +1,8 @@
 ﻿$(document).ready(function() {
     if (window.Polymer === undefined)
-        window.location.href = "/Static/BadBrowser.html";
+	    window.location.href = "/Static/BadBrowser.html";
+    else {
+    	if (!window.DOMTokenList) // IE9 detection (Polymer is defined there, but won't work)
+    		window.location.href = "/Static/BadBrowser.html";
+    }
 });
