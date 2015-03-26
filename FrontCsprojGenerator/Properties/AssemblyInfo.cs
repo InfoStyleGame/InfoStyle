ff523431ebc2df007b34b21b3fdc372a8a98e2b6
pesterev da <pesterev_da@skbkontur.ru>
2015-03-26 08:57:20 +0500

    Фронт на одной статике При этом остается автосгенерированный csproj для любителей студии (смотри readme)

--- FrontCsprojGenerator/Properties/AssemblyInfo.cs
+++ FrontCsprojGenerator/Properties/AssemblyInfo.cs
@@ -1,14 +1,15 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("infostyle")]
+[assembly: AssemblyTitle("Generator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("infostyle")]
+[assembly: AssemblyProduct("Generator")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("adeae2fc-e2af-41ed-ac83-c0319a6ccd64")]
+[assembly: Guid("d24997bf-f4bc-4cdf-ab54-e5514eb01546")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -28,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- infostyle/Exceptions/LoginPleaseException.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace infostyle.Exceptions
-{
-    public class LoginPleaseException : Exception
-    {
-        public LoginPleaseException() : base("User must login to view this page") { }
-    }
-}
